2af0bbf44fbe588de552bb5ac00a7c274653f00c
Infamy <65705916+Freeloot@users.noreply.github.com>
2022-03-14 17:24:58 +0000

    Update macspoof.py

--- macspoof.py
+++ macspoof.py
@@ -1,19 +1,21 @@
 import random
 import subprocess
 
-def Main():
-
+if __name__ == '__main__':
     interface = 'eth0'
-    random_mac = "%02x:%02x:%02x:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    new_mac = input("Enter your new MAC: ")
 
-    f = open("macs.txt", "a")
-    f.write("-> Generated MAC: " + random_mac + '\n')
-    f.close()
-    print("-> Generated MAC: " + random_mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-    print("[+] Changing MAC address for " + interface + " to " + random_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", random_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    try:
+        subprocess.call(["ifconfig", interface, "down"])
+        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+        subprocess.call(["ifconfig", interface, "up"])
 
-Main()
+        f = open("macs.txt", "a")
+        f.write("-> Assigned MAC: " + new_mac + '\n')
+        f.close()
+
+        print("-> Assigned MAC: " + new_mac)
+    except Exception as error:
+        print(error)
